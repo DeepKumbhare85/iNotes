bcf2a35bd5db4e01cd47b4c099b86276ba472e0a
deep <kumbharedeep85@gmail.com>
2022-01-06 23:02:41 +0530

    connected to database notes successfully

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
+<?php
+  $hostname="localhost";
+  $username="root";
+  $password="";
+  $dbname="notes";
 
+  $conn=mysqli_connect($hostname,$username,$password,$dbname);
 
-
-
-
-
-
-
-
+  if(!$conn){
+    die("sorry we cant connect to database".mysqli_error());
+  }
+  
+?>
 <!doctype html>
 <html lang="en">
   <head>
